f396ce14e126980027b9048b90710b270cf51da5
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-03-31 03:51:17 +0100

    Add modules for episodes, comments, characters, and locations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { EpisodeModule } from './episodes/episodes.module';
+import { CommentModule } from './comments/comments.module';
+import { CharacterModule } from './characters/characters.module';
+import { LocationModule } from './locations/locations.module';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, EpisodeModule, CharacterModule, CommentModule, LocationModule],
 })
 export class AppModule {}
